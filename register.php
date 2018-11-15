a6250ddeab6989916f673ad0e4a3f442b9a03886
TeddyGuo <error35643527@gmail.com>
2018-11-15 16:43:29 +0800

    finish the basic version of the websites

--- register.php
+++ register.php
@@ -18,7 +18,87 @@
 		<link href="/css/style.css" rel="stylesheet" />
         <script src="/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
     </head>
-    <body>
-        
-    </body>
+    <body onLoad="permissionText(document.getElementById('permission'))">
+		<header class="masthead">
+			<div class="container">
+				<div class="masthead-logo">
+					NTUST-ptt
+				</div>
+				<nav class="masthead-nav">
+					<a href="/index.html">Back</a>
+				</nav>
+			</div>
+		</header>
+		
+		<div class="container markdown-body">
+			<h1 class="page-title">Register a new account</h1>
+			<form method="post" action="_register.php" onSubmit="return inputCheck()">
+				<p>
+					<label for="username">Username :</label>
+					<input class="form-control" id="username" name="username" type="text" />
+				</p>
+				<p>
+					<label for="password">Password :</label>
+					<input class="form-control" id="password" name="password" type="password" />
+				</p>
+				<p>
+					<label for="confirm">Confirm password :</label>
+					<input class="form-control" class="form-control" id="confirm" type="password" />
+				</p>
+				<p>
+					<label for="permission">Default permission :</label>
+					<select class="form-control" id="permission" name="permission" autoComplete="off" onChange="permissionText(this)">
+						<?php echo($option); ?>
+					</select>
+				</p>
+				<p><div id="describe"></div></p>
+				<input class="btn" type="submit" name="submit" value="Register" />
+			</form>
+			<footer class="footer">
+				We are family!
+			</footer>
+		</div>
+	</body>
 </html>
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																										
+<script>
+    function inputCheck()
+    {
+        username = document.getElementById("username");
+        password = document.getElementById("password");
+        confirm = document.getElementById("confirm");
+        if (!username.value)
+        {
+            alert("User name should not be empty.");
+            username.focus();
+            return false;
+        }
+        if (!password.value)
+        {
+            alert("Password should not be empty.");
+            password.value = "";
+            confirm.value = "";
+            password.focus();
+            return false;
+        }
+        if (password.value != confirm.value)
+        {
+            alert("Passwords should be consistent.");
+            password.value = "";
+            confirm.value = "";
+            password.focus();
+            return false;
+        }
+        return true;
+    }
+    function permissionText(select)
+    {
+        permissionToText = new Array();
+        permissionToText[0] = "A tourist can only read posts.";
+        permissionToText[1] = "A user can read, create posts, but cannot delete posts of others.";
+        permissionToText[2] = "A moderator can read, create and delete posts.";
+        permissionToText[3] = "An administrator can read, create and delete posts. \
+                        An administrator can also manage the status of others.";
+        document.getElementById("describe").innerHTML = permissionToText[select.selectedIndex];
+    }
+</script>
