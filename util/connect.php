62cf59dd667d37aa1beb409791ddec7f08e75f2c
TeddyGuo <error35643527@gmail.com>
2018-12-08 00:11:06 +0800

    change password to connect db on heroku

--- util/connect.php
+++ util/connect.php
@@ -1,5 +1,5 @@
 <?php
-	$con = new mysqli("127.0.0.1", "root", "newpassword", "HEHE") or die('Error while selecting database: '.mysqli_connect_error());
+	$con = new mysqli("127.0.0.1", "root", "2e2a4848", "HEHE") or die('Error while selecting database: '.mysqli_connect_error());
 	// version php5
 	/*
 	$con = mysql_connect('127.0.0.1', 'root', 'newpassword') or die('Error while connecting SQL: '.mysql_error());
