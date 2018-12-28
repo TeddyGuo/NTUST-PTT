df2882784f86a839512e76ea2ab20a934911efc1
TeddyGuo <error35643527@gmail.com>
2018-12-28 17:16:24 +0800

    make db correct

--- util/connect.php
+++ util/connect.php
@@ -1,5 +1,6 @@
 <?php
-	$con = new mysqli("127.0.0.1", "root", "newpassword", "HEHE") or die('Error while selecting database: '.mysqli_connect_error());
+	$con = new mysqli("us-cdbr-iron-east-01.cleardb.net", "b3814a2aa0f0f8", "2e2a4848", "heroku_cc89c495e6357c1") or die('Error while selecting database: '.mysqli_connect_error());
+	// $con = new mysqli("us-cdbr-iron-east-01.cleardb.net", "b3814a2aa0f0f8", "2e2a4848", "heroku_cc89c495e6357c1") or die('Error while selecting database: '.mysqli_connect_error());
 	// version php5
 	/*
 	$con = mysql_connect('127.0.0.1', 'root', 'newpassword') or die('Error while connecting SQL: '.mysql_error());
