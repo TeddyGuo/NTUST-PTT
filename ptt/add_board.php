9c9e7a57d9037a88f97711e7ffe31ffa60fec11d
TeddyGuo <error35643527@gmail.com>
2019-01-02 21:46:54 +0800

    add_board.php

--- ptt/add_board.php
+++ ptt/add_board.php
@@ -8,10 +8,11 @@
     if (!($_SESSION['default_permission'] >= MODERATOR))
         exit('Not enough permission.');
         
+    $user_id = $_SESSION['user_id'];
     $board_name = $_POST['board_name'];
     $board_name = addslashes($board_name);
     $board_name = $con->real_escape_string($board_name);
-    $query = "INSERT INTO board(board_name) VALUES ('$board_name')";
+    $query = "INSERT INTO board(board_name) VALUES ('$board_name', $user_id)";
     $con->query($query) or die($query . '<br/>' . $con->error);
     $last_page = $_SERVER["HTTP_REFERER"];
     header("Location: $last_page");
