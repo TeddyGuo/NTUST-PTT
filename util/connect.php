e57a914d87a0ef721df9eda396b208c75c50294f
TeddyGuo <error35643527@gmail.com>
2019-01-02 16:10:34 +0800

    make it better

--- util/connect.php
+++ util/connect.php
@@ -2,8 +2,8 @@
 	// $con = new mysqli("127.0.0.1", "root", "newpassword", "HEHE") or die('Error while selecting database: '.mysqli_connect_error());
 	$con = new mysqli("us-cdbr-iron-east-01.cleardb.net", "b3814a2aa0f0f8", "2e2a4848", "heroku_cc89c495e6357c1") or die('Error while selecting database: '.mysqli_connect_error());
 
-	$con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	// $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	// version php5
 	/*
 	$con = mysql_connect('127.0.0.1', 'root', 'newpassword') or die('Error while connecting SQL: '.mysql_error());
